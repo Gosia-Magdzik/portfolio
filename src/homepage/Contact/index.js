31b9eb64063e4e382e1b8c90b0c77f4672ec0165
Gosia Magdzik <melassa26@gmail.com>
2023-04-17 17:07:18 +0200

    contact style details

--- src/homepage/Contact/index.js
+++ src/homepage/Contact/index.js
@@ -1,19 +1,21 @@
-import { StyledContact, StyledContactItem } from "./styled";
+import { StyledContact, StyledContactItem, ContactCircle } from "./styled";
 import { Link } from "./Link";
 
 
 export const Contact = () => (
     <StyledContact>
         <StyledContactItem>
+            <ContactCircle/>
             <Link
                 href={"mailto:melassa26@gmail.com"}
-                description={"📧 melassa26@gmail.com"}
+                description={"melassa26@gmail.com"}
             />
         </StyledContactItem>
         <StyledContactItem>
+            <ContactCircle/>
             <Link
                 href={"tel:+48794753888"}
-                description={"📞 (+48) 794-753-888"}
+                description={"(+48) 794-753-888"}
             />
         </StyledContactItem>
     </StyledContact>
--- src/homepage/Contact/styled.js
+++ src/homepage/Contact/styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledLink = styled.a`
     text-decoration: none;
     color: black;
+    font-size: larger;
 
   &:hover {
     font-style: italic;
@@ -10,13 +11,24 @@ export const StyledLink = styled.a`
 `;
 
 export const StyledContact = styled.ul`
-    padding: 5px;
     min-width: 70px;
     text-align: left;
-
+    margin-left: 1em;
 `;
 
 export const StyledContactItem = styled.li`
+    display: flex;
     list-style-type: none;
     min-width: 50vh;
+    margin-bottom: 0.5em;
 `;
+
+export const ContactCircle = styled.div`
+    width: 1em;
+    height: 1em;
+    margin-right: 0.5em;
+    border-radius: 10em;
+    background-color: #df5f5f;
+    margin-bottom: 0em;
+`;
+   
