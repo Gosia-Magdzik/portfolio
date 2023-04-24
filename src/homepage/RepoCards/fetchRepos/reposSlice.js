a60bcd0595dbd186a92f62d3dc1297bdf9677176
Gosia Magdzik <melassa26@gmail.com>
2023-04-24 15:26:21 +0200

    redux Slice component added

---
+++ src/homepage/RepoCards/fetchRepos/reposSlice.js
@@ -0,0 +1,31 @@
+//akction
+import { createSlice } from "@reduxjs/toolkit";
+
+export const reposSlice = createSlice({
+    name: "repos",
+    initialState: {
+        status: "",
+        repos: []
+    },
+    reducers: {
+        fetchRepos: state => {
+            state.status = "loading";
+        },
+        fetchReposSuccess: (state, {payload: repos}) => {
+            state.repos = repos;
+            state.status = "success"
+        },
+        fetchError: state => {
+            state.status = "error";
+        }
+    },
+});
+
+export const { fetchRepos,
+               fetchReposSuccess,
+               fetchError } = reposSlice.actions;
+
+export const selectReposStatus = state => selectState(state).status;
+export const selectRepos = state => selectState(state).repos;
+
+export default reposSlice.reducer;
