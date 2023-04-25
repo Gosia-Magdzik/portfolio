e88bfab405c4e11dc41203ceddb8106538e2ad23
Gosia Magdzik <melassa26@gmail.com>
2023-04-25 15:51:51 +0200

    RepoCards created and styled

--- src/homepage/RepoCards/index.js
+++ src/homepage/RepoCards/index.js
@@ -3,5 +3,67 @@ import { useDispatch, useSelector } from "react-redux";
 import { nanoid } from "nanoid";
 import Card from "react-bootstrap/Card";
 import { Row, Col } from "react-bootstrap";
+import { fetchRepos, selectRepos, selectReposStatus } from "./fetchRepos/reposSlice";
+import { StyledButton, StyledCard } from "./styled"
+import { Error } from "./Error";
+import { Loader } from "./Loader";
 
-//loader, Error, styled, fetch
+export const ReposCards = () => {
+    const dispatch = useDispatch();
+    const repos = useSelector(selectRepos);
+    const status = useSelector(selectReposStatus);
+
+    useEffect(() => {
+        dispatch(fetchRepos());
+    }, [dispatch])
+
+    return(
+			<>
+			{	status === "loading" ? <Loader /> :
+				status === "error" ? <Error /> :
+        <Row
+            xs={1}
+            md={2}
+            lg={3}
+        >
+            {repos.map(repo => (
+                repo.homepage &&
+                <Col
+                  key={nanoid()}
+                >
+                  <Card as={StyledCard}>
+                    <Card.Body>
+                      <Card.Title>{repo.name}</Card.Title>
+                      <Card.Text
+											  style={{
+													overflow: "auto",
+													height: "100px"
+												}}
+											>
+                        {repo.description}
+                      </Card.Text>
+											<Row>
+												<Col>
+													<StyledButton
+														href={repo.homepage}
+													>
+														Demo
+													</StyledButton>											
+												</Col>
+												<Col>
+													<StyledButton
+														href={repo.html_url}
+													>
+														Code
+													</StyledButton>											
+												</Col>         												
+											</Row>            
+                    </Card.Body>
+                  </Card>
+                </Col>
+            ))}
+        </Row>
+			}
+			</>
+    );
+};
---
+++ src/homepage/RepoCards/styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import  Button  from "react-bootstrap/Button";
+import { Card } from "react-bootstrap"
+
+export const StyledButton = styled(Button).attrs(() => ({
+    className: "w-100",
+    target: "_blank",
+    //rel: "noopener noreferrer"
+  }))`
+    
+    border: none;
+  
+    //&:hover {
+    //&.btn-primary:active {
+  `;
+
+export const StyledCard = styled(Card)`
+  width: 300px; 
+  border-radius: 20px;
+  background-color: #CAF2EE; 
+`;
