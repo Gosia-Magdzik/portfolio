1a4ad68758d4cf0c0340c6d59e23ab67af7092ba
Gosia Magdzik <melassa26@gmail.com>
2023-04-25 16:38:44 +0200

    Header adjust

--- src/homepage/Header/index.js
+++ src/homepage/Header/index.js
@@ -17,23 +17,17 @@ function Header() {
                     />
                 </Col>
                 <Col className="align-items-center text-center justify-content-center">
-                    <Row sm={4}  
-                         className="mt-1 mb-2">
+                    <Row  className="mt-3 mb-2">
                         <h2>
                             Małgorzata Magdzik
                         </h2>
                     </Row >
-                    <Row    sm={3} 
-                            className="mb-2"
+                    <Row  className="mb-2"
                     >
                         <p> Front end developer </p>
                     </Row>
-                    <br/>
-                    <Row sm={3} >
-                        <Contact/>
-                    </Row>
-                    <Row sm={3}>
-
+                    <Row >
+                           <Contact/> 
                     </Row>
                 </Col>                       
              </Row>
