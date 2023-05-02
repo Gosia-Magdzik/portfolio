02ba592441788eaa95b4e50a4a39dda6e1086dac
Gosia Magdzik <melassa26@gmail.com>
2023-05-02 18:15:45 +0200

    contact area styled

--- src/homepage/Contact/index.js
+++ src/homepage/Contact/index.js
@@ -1,21 +1,25 @@
-import { StyledContact, StyledContactItem, ContactCircle } from "./styled";
+import { StyledContact, StyledContactItem, Image } from "./styled";
 import { Link } from "./Link";
+import Envelope from "./envelope.svg"
+import WhatsApp from "./whatsapp.svg"
 
 
 export const Contact = () => (
     <StyledContact>
         <StyledContactItem>
-            <ContactCircle/>
+            <Image src={Envelope} />
+            &nbsp;&nbsp;
             <Link
                 href={"mailto:melassa26@gmail.com"}
-                description={"melassa26@gmail.com"}
+                description={" melassa26@gmail.com"}
             />
         </StyledContactItem>
         <StyledContactItem>
-            <ContactCircle/>
+            <Image src={WhatsApp} />
+            &nbsp;&nbsp;
             <Link
                 href={"tel:+48794753888"}
-                description={"(+48) 794-753-888"}
+                description={" (+48) 794-753-888"}
             />
         </StyledContactItem>
     </StyledContact>
--- src/homepage/Contact/styled.js
+++ src/homepage/Contact/styled.js
@@ -4,34 +4,35 @@ export const StyledLink = styled.a`
     text-decoration: none;
     color: black;
     font-size: max(2vw, 20px);
-    word-wrap: break-word !important;
+    word-wrap: break-word;
     margin-top: 20px;
-
-  &:hover {
-    font-style: italic;
-  };
 `;
 
 export const StyledContact = styled.ul`
     text-align: left;
     margin-left: 1em;
+    margin-right: 1em;
     word-wrap: break-word;
+    border: 1px solid;
+    border-radius: 30px;
+    background-color: rgba(241,241,222, 0.4);
 `;
 
 export const StyledContactItem = styled.li`
     display: flex;
     list-style-type: none;
-    min-width: 50vh;
     margin-bottom: 0.5em;
     word-wrap: break-word;
+
+    &:hover {
+      font-style: italic;
+      scale: 1.1;
+  };
 `;
 
-export const ContactCircle = styled.div`
-    width: 1em;
-    height: 1em;
-    margin-right: 0.5em;
-    border-radius: 10em;
-    background-color: #df5f5f;
-    margin-bottom: 0em;
+export const Image = styled.img`
+    width: 3.5em;
+    height: 3.5em;
+    align-items: flex-end;
 `;
    
