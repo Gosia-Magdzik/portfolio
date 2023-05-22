56a33121d2fcfaf573f84755f8b0fd36dec80547
Gosia Magdzik <melassa26@gmail.com>
2023-05-22 11:25:49 +0200

    motion added

--- src/homepage/Contact/index.js
+++ src/homepage/Contact/index.js
@@ -2,6 +2,7 @@ import { StyledContact, StyledContactItem, Image } from "./styled";
 import { Link } from "./Link";
 import Email from "./email.svg"
 import WhatsApp from "./whatsapp.svg"
+import { Motion } from "../Header/Motion"
 
 
 export const Contact = () => (
@@ -9,18 +10,30 @@ export const Contact = () => (
         <StyledContactItem>
             <Image src={Email} />
             &nbsp;&nbsp;
+            <Motion
+                y={-500}
+                type={"spring"}
+                mass={5}
+                animatedElement={ 
             <Link
                 href={"mailto:magdzik.frontend@gmail.com"}
                 description={"magdzik.frontend@gmail.com"}
             />
+        }/>
         </StyledContactItem>
         <StyledContactItem>
             <Image src={WhatsApp} />
             &nbsp;&nbsp;
-            <Link
-                href={"tel:+48794753888"}
-                description={"(+48) 794-753-888"}
-            />
+            <Motion
+                y={-500}
+                type={"spring"}
+                mass={5}
+                animatedElement={ 
+                    <Link
+                        href={"tel:+48794753888"}
+                        description={"(+48) 794-753-888"}
+                    />
+            }/>
         </StyledContactItem>
     </StyledContact>
 );
