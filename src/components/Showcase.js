0e3856b9f0c5426ab4099c4fcae17680afed5d56
Gosia Magdzik <melassa26@gmail.com>
2023-12-08 21:13:05 +0100

    new component folder has been created

---
+++ src/components/Showcase.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    PaddingContainer,
+    FlexContainer,
+    Heading,
+    ParaText,
+    BlueText,
+    IconContainer,
+} from '../styles/Global.styled';
+
+const Showcase = () => {
+  return (
+    <div>Showcase</div>
+  )
+}
+
+export default Showcase
