b4cbe15c4cd4a54a36604a25a5dbef3792f178a9
Gosia Magdzik <melassa26@gmail.com>
2023-12-08 22:17:21 +0100

    heading styling

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -7,6 +7,7 @@ import {
     BlueText,
     IconContainer,
 } from '../styles/Global.styled';
+import { FaGithub } from "react-icons/fa";
 
 export const Showcase = () => {
   return (
@@ -17,14 +18,25 @@ export const Showcase = () => {
         top="15%"
         bottom="10%"
     >
-        <FlexContainer
-            align="center"
-            fullWidthChild
-        >
+        <FlexContainer align="left" fullWidthChild>
+            {/* --left-content-- */}
             <div>
-                <Heading size="h4">
-                    Hello!
+                <Heading as="h4" size="h4">Hello!</Heading>
+                <Heading as="h1" size="h1" top="0.5rem" bottom="1rem">
+                    I'm <BlueText>Malgorzata Magdzik</BlueText> 
                 </Heading>
+                <Heading as="h3" size="h3">
+                    I'm a <BlueText>Frontend Developer</BlueText>
+                </Heading>
+                <ParaText as="p" top="2rem" bottom="4rem">
+                    Hello, I am a passionate coder, looking for a frontend developer role
+                </ParaText>
+
+                <FlexContainer>
+                    <IconContainer color="blue" size="3rem">
+                        <FaGithub/>
+                    </IconContainer>
+                </FlexContainer>
             </div>
         </FlexContainer>
     </PaddingContainer>
