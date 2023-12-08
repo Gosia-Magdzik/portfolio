22ae7d604b09ffae231ea187e086b0ab650f2726
Gosia Magdzik <melassa26@gmail.com>
2023-12-08 20:10:56 +0100

    props added

--- src/styles/Global.styled.js
+++ src/styles/Global.styled.js
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 
 export const MainBody = styled.div`
-    background-color: #000;
-    height: 50px;
-    width: 100%;
+    background-color: ${({ theme }) => theme.colors.primary};
 `;
