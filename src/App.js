65cdeb40c2f592eed2631e438be9a308c1399847
Gosia Magdzik <melassa26@gmail.com>
2023-12-10 14:42:25 +0100

    nav component styling + useState hook added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { Container, MainBody } from './styles/Global.styled';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './utils/Theme';
+
 import { Showcase } from './components/Showcase';
 import { Myskills } from './components/Myskills';
 import { MyProjects } from './components/MyProjects';
 import { Footer } from './components/Footer';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <MainBody>
+        <Navbar />
         <Container>
           <Showcase/>
           <Myskills/>
---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import {
+    PaddingContainer,
+    Container,
+    FlexContainer,
+    BlueText,
+} from '../styles/Global.styled';
+import { NavbarContainer,
+        Logo,
+        MenuIcon,
+} from '../styles/Navbar.styled';
+
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { theme } from '../utils/Theme'
+
+export const Navbar = () => {
+
+    const [openMenu, setOpenMenu] = useState(false);
+
+
+  return (
+    <NavbarContainer bgColor="transparent">
+            <PaddingContainer
+                top="1.2rem"
+                bottom="1.2rem"
+            >
+                <Container>
+                    <FlexContainer justify="space-between">
+                        <Logo>JustA<BlueText>Dev</BlueText></Logo>
+                        <MenuIcon
+                            onClick={() => { setOpenMenu(true) }}
+                        >
+                            <GiHamburgerMenu/>
+                        </MenuIcon>
+                    </FlexContainer>
+                </Container>
+
+                {openMenu && <NavMenu/>}
+            </PaddingContainer>
+    </NavbarContainer>
+  )
+}
+
