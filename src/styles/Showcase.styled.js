0dfa407271c78f74aded965f3ab06184e96a57ca
Gosia Magdzik <melassa26@gmail.com>
2023-12-12 16:45:33 +0100

    img replaced

--- src/styles/Showcase.styled.js
+++ src/styles/Showcase.styled.js
@@ -5,11 +5,20 @@ export const ShowcaseParticleContainer = styled.div`
 `
 
 export const ShowcaseImageCard = styled.div`
-    width: max-content;
+    height: 30rem;
+    width: 100%;
     padding-top: 2rem;
-    border-radius: 3rem;
+    border-radius: 1rem;
     position: relative;
     overflow: hidden;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile }){
+        padding-top: 1rem;
+        height: 30rem;
+        display: block;
+        border-radius: 50%;
+
+    }
 `
 
 export const Particle = styled.img`
